6da6b07f030d9b225f76c69057dc98baeec913ed
sglzf <36081915+sglzf@users.noreply.github.com>
2018-11-05 20:39:14 +0100

    Update BBCON.py

---
+++ BBCON.py
@@ -0,0 +1,51 @@
+from time import sleep
+class BBCON():
+    
+    def __init__(self): 
+        self.behaviors = [] #list of all behaviors
+        self.active_behaviors = [] #list of all active behaviors
+        self.sensobs = [] #list of all sensory objects used by the bbcno
+        self.motobs = [] #list of all motors objects used by the bbcon
+        self.arbitrator = None #arbitrator object that will resolve actuator requests produced by the behaviors
+        #ikke lagt til forslag om timestep, inactive o.l.
+    
+    #metoder for bbcon
+    def add_behavior(self,behavior):
+        self.behaviors.append(behavior)
+
+    def activate_behavior(self,behavior):
+        if behavior not in self.behaviors:
+            raise Exception("må ha adferden som forsøkes aktivert")
+        self.active_behaviors.append(behavior)
+
+    def deactive_behavior(self, behavior):
+        self.active_behaviors.remove(behavior)
+
+    def add_sensob(self,sensob):
+        self.sensobs.append(sensob)
+    
+    #mainmetode/kjerneaktivitet i bbcon
+    def run_one_timestep(self):
+        self.update_all_sensobs() #1. oppdater alle sensobs
+        self.update_all_behaviors() #2. oppdater alle behaviors
+        motor_recommendations = self.arbitrator.choose_action() #3. invoke arbitrator by caling choose action, mellomlagre output
+        self.update_motobs(motor_recommendations) #4.update settings of all motors 
+        self.wait(0.5) #5. pause for å la motorsettings være aktive en periode
+        self.reset_sensobs() #6. 
+
+
+    #støttefunksjoner til run_one_timestep 
+    def update_all_sensobs(self):
+        raise NotImplementedError
+
+    def update_all_behaviors(self):
+        raise NotImplementedError
+    
+    def update_motobs(self, recommendations):
+        raise NotImplementedError
+
+    def wait(self, seconds):
+        sleep(seconds) #står eksempelvis halvt sek,må sikkert justeres
+
+    def reset_sensobs(self):
+        raise NotImplementedError
